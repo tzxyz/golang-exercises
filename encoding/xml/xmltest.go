477b74e65a7c07a6588224bb97026aa1a73a7f8a
zhuonima <vincent.fml@hotmail.com>
2015-05-16 20:11:50 +0800

    update

--- encoding/xml/xmltest.go
+++ encoding/xml/xmltest.go
@@ -9,6 +9,7 @@ type Person struct {
 	XMLName xml.Name `xml:"person"`
 	Names   []Name   `xml:"name"`
 	Id      string   `xml:"name,attr"`
+	Comment string   `xml:",comment"`
 }
 
 type Name string
@@ -18,15 +19,18 @@ func main() {
 
 	var name1 Name = "111"
 	var name2 Name = "222"
+
 	names = append(names, name1)
 	names = append(names, name2)
 
 	person := &Person{
-		Names: names,
-		Id:    "11",
+		Names:   names,
+		Id:      "11",
+		Comment: "注释",
 	}
 
 	indent := "    "
+
 	data, err := xml.MarshalIndent(person, indent, indent)
 	if err != nil {
 		fmt.Println(err)
